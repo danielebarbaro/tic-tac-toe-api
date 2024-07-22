ca37b77d84f18ed174528df8650b7211c8126177
Daniele Barbaro <barbaro.daniele@gmail.com>
2024-07-23 00:10:06 +0200

    reset database for tests

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
